4a9f493d631e9ddf9a363b5f6f466e428e3c235d
Your Name <you@example.com>
2023-10-18 23:04:31 +0200

    signature

--- main.js
+++ main.js
@@ -9,32 +9,36 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 context = canvas.getContext('2d');
 
-alert("start");
+
 signature(50, 50, 50);
 
 function signature(x, y, size) {
 
     context.fillStyle = "black";
     context.fillRect(x + 425, y + 250, 300, 300);
-    context.fillStyle = "cyan"
+    context.fillStyle = "lightblue"
 
 
     context.fillRect(x + 550, y + 250, size, size);
     context.fillRect(x + 500, y + 300, size, size);
-    context.fillRect(x + 450, y + 300, size, size);
-    context.fillRect(x + 600, y + 300, size, size);
-    context.fillRect(x + 650, y + 300, size, size);
     context.fillRect(x + 550, y + 300, size, size);
-
+    context.fillRect(x + 600, y + 300, size, size);
     context.fillRect(x + 550, y + 350, size, size);
-    context.fillRect(x + 500, y + 500, size, size);
-    context.fillRect(x + 500, y + 450, size, size);
-    context.fillRect(x + 500, y + 400, size, size);
+    context.fillRect(x + 600, y + 350, size, size);
+    context.fillRect(x + 650, y + 350, size, size);
+
     context.fillRect(x + 550, y + 400, size, size);
     context.fillRect(x + 600, y + 400, size, size);
+    
+    context.fillRect(x + 500, y + 350, size, size);
+    context.fillRect(x + 450, y + 350, size, size);
+    context.fillRect(x + 500, y + 400, size, size);
+    context.fillRect(x + 500, y + 450, size, size);
+    context.fillRect(x + 450, y + 450, size, size);
+    context.fillRect(x + 550, y + 450, size, size);
     context.fillRect(x + 600, y + 450, size, size);
-    context.fillRect(x + 600, y + 500, size, size);
-
+    context.fillRect(x + 650, y + 450, size, size);
+    
 
 
 }
